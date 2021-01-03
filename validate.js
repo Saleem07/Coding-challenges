81005cd339953530cc3af33a16509666f7208695
Saleem <mohammadsaleem07@outlook.com@example.com>
2021-01-03 13:28:13 +0530

    will check for 6 and more charcters

--- validate.js
+++ validate.js
@@ -8,7 +8,7 @@
 // *Valid passwords will only be alphanumeric characters.
 
 function validate(password) {
-  return /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]){6,}/.test(password);
+  return /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{6,}/.test(password);
 }
 
 let output = validate("dj38");
